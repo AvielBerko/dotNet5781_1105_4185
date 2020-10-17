16721458c2883a1d5c216dc989ad1fedc37caa4c
Aviel Berkowitz <9155988@gmail.com>
2020-10-17 23:18:10 +0300

    Wev'e created a new solution

---
+++ dotNet_5781_1105_4185/dotNet_5781_00_1105_4185/Program1105.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet_5781_00_1105_4185
+{
+	partial class Program
+	{
+		static void Main(string[] args)
+		{
+			Welcome1105();
+			Welcome4185();
+			Console.ReadKey();
+		}
+
+		static partial void Welcome4185();
+		private static void Welcome1105()
+		{
+			Console.WriteLine("Enter your name: ");
+			string name = Console.ReadLine();
+			Console.WriteLine("{0}, welcome to my first console application", name);
+		}
+	}
+}
