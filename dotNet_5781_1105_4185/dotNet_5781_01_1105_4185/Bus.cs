b388b4c7075cba49a28fb527e435d85a40d3c629
Aviel Berkowitz <9155988@gmail.com>
2020-10-24 23:56:00 +0300

    Program finished

--- dotNet_5781_1105_4185/dotNet_5781_01_1105_4185/Bus.cs
+++ dotNet_5781_1105_4185/dotNet_5781_01_1105_4185/Bus.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,12 @@ namespace dotNet_5781_01_1105_4185
 {
 	class Bus
 	{
+		public Bus(string reg, DateTime date)
+		{
+			DateRegistered = date;
+			Registration = reg;
+			KmToRefuel = 1200;
+		}
 		public DateTime DateRegistered { get; private set; }
 
 		public string Registration
@@ -15,9 +22,22 @@ namespace dotNet_5781_01_1105_4185
 			get => registration;
 			set
 			{
-				if (DateRegistered.Year >= 2018 && value.Length == 8 || value.Length == 7)
+				if (uint.TryParse(value, out _))
 				{
-					registration = value;
+					if (DateRegistered.Year >= 2018 && value.Length == 8 ||
+						DateRegistered.Year < 2018 && value.Length == 7)
+					{
+						registration = value;
+						//registration = string.Format("{0}-{1}-{2}", value.Substring(0, 3), value.Substring(3, 2), value.Substring(5, 3));
+					}
+					/*else if (value.Length == 7)
+					{
+						registration = string.Format("{0}-{1}-{2}", value.Substring(0, 2), value.Substring(2, 3), value.Substring(4, 2));
+					}*/
+					else
+					{
+						throw new Exception("Invalid registration");
+					}
 				}
 				else
 				{
@@ -29,6 +49,41 @@ namespace dotNet_5781_01_1105_4185
 
 		public uint Kilometrage { get; private set; }
 
-		private uint lastKmTreatment;
+		private uint lastTreatmentKm;
+		private DateTime lastTreatmentDate;
+		public bool Risky => (Kilometrage - lastTreatmentKm > 20000);
+		public uint KmToRefuel { get; private set; }
+
+		public void Treatment()
+		{
+			lastTreatmentDate = DateTime.Now;
+			lastTreatmentKm = Kilometrage;
+		}
+		public void Refuel()
+		{
+			KmToRefuel = 1200;
+		}
+		public void Drive(uint km)
+		{
+			if (KmToRefuel >= km)
+			{
+				KmToRefuel -= km;
+				Kilometrage += km;
+			}
+			else
+			{
+				throw new Exception("Cannot drive the distance, refuel needed");
+			}
+		}
+		public override string ToString()
+		{
+			string fmtReg;
+			if (registration.Length == 8)
+				fmtReg = $"{registration.Substring(0, 3)}-{registration.Substring(3, 2)}-{ registration.Substring(5, 3)}";
+			else
+				fmtReg = $"{registration.Substring(0, 2)}-{registration.Substring(2, 3)}-{ registration.Substring(4, 2)}";
+
+			return $"Registration Number: {fmtReg}, Km since last treatment: {Kilometrage - lastTreatmentKm}";
+		}
 	}
 }
--- dotNet_5781_1105_4185/dotNet_5781_01_1105_4185/Program.cs
+++ dotNet_5781_1105_4185/dotNet_5781_01_1105_4185/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,97 @@ namespace dotNet_5781_01_1105_4185
 {
 	class Program
 	{
+		static Random rand = new Random();
+		enum Menu { INSERT = 1 , BUS_SELECT, REFUEL_TREATMENT, LAST_TREATMENT_KM, EXIT }
+		enum ActionsMenu { TREATMENT = 1, REFUEL}
 		static void Main(string[] args)
 		{
+			Console.WriteLine("Select 1-5:\n" + "1: Insert a new bus to the database\n" +
+							 "2: Select a bus to drive\n" + "3: Refuel or send to treatment\n" +
+							 "4: Print all buses last treatment Kilometrage\n" + "5: Exit");
+			List<Bus> buses = new List<Bus>();
+			Menu choice = Menu.EXIT;
+			do
+			{
+				try
+				{
+					if (Enum.TryParse(Console.ReadLine(), out choice))
+					{
+
+						switch (choice)
+						{
+							case Menu.INSERT:
+								{
+									Console.Write("Enter registration-number: ");
+									string regnum = Console.ReadLine();
+									Console.Write("Enter date registrated: ");
+									if (DateTime.TryParse(Console.ReadLine(), out DateTime date))
+										buses.Add(new Bus(regnum, date));
+									else
+										throw new Exception("Date format is ivalid, try dd/mm/yyyy");
+									break;
+								}
+							case Menu.BUS_SELECT:
+								{
+									Console.Write("Enter registration-number: ");
+									string regnum = Console.ReadLine();
+									Bus selectedBus = buses.Find((bus) => (regnum == bus.Registration));
+									if (selectedBus != null)
+										selectedBus.Drive((uint)rand.Next(1, 1200));
+									else
+										throw new Exception("Bus was not found");
+									break;
+								}
+							case Menu.REFUEL_TREATMENT:
+								{
+									Console.Write("Enter registration-number: ");
+									string regnum = Console.ReadLine();
+									Bus selectedBus = buses.Find((bus) => (regnum == bus.Registration));
+									if (selectedBus != null)
+									{
+										Console.WriteLine("Enter 1 for treatment OR 2 to refuel:");
+										if (Enum.TryParse(Console.ReadLine(), out ActionsMenu action))
+										{
+											switch (action)
+											{
+												case ActionsMenu.TREATMENT:
+													selectedBus.Treatment();
+													break;
+												case ActionsMenu.REFUEL:
+													selectedBus.Refuel();
+													break;
+												default:
+													break;
+											}
+										}
+										else
+											throw new Exception("Invalid choice");
+									}
+									else
+										throw new Exception("Bus was not found");
+									
+									break;
+								}
+							case Menu.LAST_TREATMENT_KM:
+								{
+									foreach (Bus bus in buses)
+										Console.WriteLine(bus);
+									break;
+								}
+							case Menu.EXIT:
+								Console.WriteLine("Program terminated with errors, Exit Code (-13452i)");
+								return;
+						}
+					}
+					else
+						throw new Exception("Invalid choice");
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine(e.Message);
+				}
+				Console.Write("Select 1-5: ");
+			} while (choice != Menu.EXIT);
 		}
 	}
 }
