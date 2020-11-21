f6a5a34c3e5cb67d39a3159cbc259f9d295c4dea
Aviel Berkowitz <9155988@gmail.com>
2020-11-21 20:02:46 +0200

    Started ex3A

---
+++ dotNet_5781_1105_4185/dotNet_5781_03A_1105_4185/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace dotNet_5781_03A_1105_4185
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+	}
+}
