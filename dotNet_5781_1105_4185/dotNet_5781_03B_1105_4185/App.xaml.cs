46f4c9c2c63b3cd37ccc0d276c788b0794631b08
Meir Klemfner <meirk251@outlook.com>
2020-12-06 17:03:51 +0200

    Created the wpf project.

---
+++ dotNet_5781_1105_4185/dotNet_5781_03B_1105_4185/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace dotNet_5781_03B_1105_4185
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
