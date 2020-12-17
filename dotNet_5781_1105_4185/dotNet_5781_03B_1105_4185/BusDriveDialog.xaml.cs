0e74c8ae1ecf5c67308221b5e8bd2453eca58edc
Meir Klemfner <meirk251@outlook.com>
2020-12-17 14:36:52 +0200

    Removed the buttons from the Driving Dialog.

--- dotNet_5781_1105_4185/dotNet_5781_03B_1105_4185/BusDriveDialog.xaml.cs
+++ dotNet_5781_1105_4185/dotNet_5781_03B_1105_4185/BusDriveDialog.xaml.cs
@@ -14,51 +14,64 @@ using System.Windows.Shapes;
 
 namespace dotNet_5781_03B_1105_4185
 {
-	/// <summary>
-	/// Interaction logic for BusDriveDialog.xaml
-	/// </summary>
-	public partial class BusDriveDialog : Window
-	{
-		public BusDriveDialog(Bus bus)
-		{
-			InitializeComponent();
+    /// <summary>
+    /// Interaction logic for BusDriveDialog.xaml
+    /// </summary>
+    public partial class BusDriveDialog : Window
+    {
+        public BusDriveDialog(Bus bus)
+        {
+            InitializeComponent();
 
-			DataContext = this;
+            DataContext = this;
 
-			this.bus = bus;
-			Title = bus.Registration.ToString();
-		}
+            this.bus = bus;
+            Title = $"Bus {bus.Registration}";
+        }
 
-		private Bus bus;
+        private Bus bus;
 
-		/// <summary>
-		/// The distance input
-		/// </summary>
-		public uint Distance { get; set; }
+        /// <summary>
+        /// The distance input
+        /// </summary>
+        public uint Distance { get; set; }
 
-		private void OkButtonClick(object sender, RoutedEventArgs e)
-		{
-			if (!bus.CanDriveDistance(Distance))
-			{
-				MessageBox.Show("Cannot drive this distance!\n" +
-					$"Can drive {bus.KmToRefuel} km until fuel tank is empty",
-					"", MessageBoxButton.OK, MessageBoxImage.Error);
-				return;
-			}
+        /// <summary>
+        /// Called when the textbox's text changed.
+        /// Checks that the input contains only digits.
+        /// </summary>
+        private void TextBoxDigitOnly(object sender, TextCompositionEventArgs e)
+        {
+            var regex = new System.Text.RegularExpressions.Regex("[^0-9]+");
+            bool isDigitOnly = !regex.IsMatch(e.Text);
+            e.Handled = !isDigitOnly;
+        }
 
-			// Closes the dialog with true as the result
-			DialogResult = true;
-		}
+        /// <summary>
+        /// Called when KeyDown event triggred in the textbox.
+        /// Closes the dialog if presesd Enter or Escape.
+        /// </summary>
+        private void KeyDownHandling(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (!bus.CanDriveDistance(Distance))
+                {
+                    MessageBox.Show("Cannot drive this distance!\n" +
+                        $"Can drive {bus.KmToRefuel} km until fuel tank is empty",
+                        "", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
 
-		/// <summary>
-		/// Called when the textboxes' text changed.
-		/// Checks that the input contains only digits.
-		/// </summary>
-		private void TextBoxDigitOnly(object sender, TextCompositionEventArgs e)
-		{
-			var regex = new System.Text.RegularExpressions.Regex("[^0-9]+");
-			bool isDigitOnly = !regex.IsMatch(e.Text);
-			e.Handled = !isDigitOnly;
-		}
-	}
+                // Closes the dialog with true as a result.
+                DialogResult = true;
+            }
+
+            if (e.Key == Key.Escape)
+            {
+                // Closes the dialog with false as a result.
+                DialogResult = false;
+            }
+        }
+    }
 }
